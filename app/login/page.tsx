f969d70faf0bdaa4c30c24d3f0300514337de82f
Rodrigo Carvalho <rodrigogoulart97@gmail.com>
2024-11-05 21:55:06 -0300

    Chore: Feita a tela de Login

---
+++ app/login/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+const LoginPage = () => {
+    return ( 
+        <div className="grid h-full grid-cols-2">
+            <div className="flex flex-col  justify-center p-8 m-w-[550px] mx-auto">
+                <Image className="mb-8" src="/logo.png" alt="Logo" width={173} height={89}/>
+                <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
+                <p className="text-muted-foreground  mb-8">A Finance AI é uma plataforma de gestão financeira que utiliza<br/>
+                IA para monitorar suas movimentações, e oferecer insights<br/>
+                personalizados, facilitando o controle do seu orçamento.</p>
+                <Button variant="outline">
+                    <LogInIcon className="mr-2"/>
+                    Fazer Login ou criar conta</Button>
+            </div>
+            <div className="relative h-full w-full">
+                <Image src="/fundo.png" alt="Faça Login" fill className="object-cover"/>
+            </div>
+        </div>
+     );
+}
+ 
+export default LoginPage;
